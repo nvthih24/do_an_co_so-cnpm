b9369be87228d65c664de178a63e0965a3c383f9
Baozxje <lamgiabao73@gmail.com>
2025-04-15 13:38:27 +0700

    employ

---
+++ job-platform-frontend/src/views/Employer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Employer = () => {
+  return (
+    <div className="container">
+      <h1>Chào mừng Nhà tuyển dụng</h1>
+      <p>Bạn đã đăng ký thành công!</p>
+    </div>
+  );
+};
+
+export default Employer;
